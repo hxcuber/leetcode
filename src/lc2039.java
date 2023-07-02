2179c23e14f8eefc9a1bff2439d50b9b8bb13377
Hoang Nguyen <hn622@ic.ac.uk>
2023-07-03 00:24:24 +0100

    2039 started

---
+++ src/lc2039.java
@@ -0,0 +1,11 @@
+public class lc2039 {
+  class Solution {
+    public int networkBecomesIdle(int[][] edges, int[] patience) {
+
+    }
+
+    private int bfsFromZero(int[][] edges, int start) {
+
+    }
+  }
+}

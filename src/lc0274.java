207837fe6a829804c93f67bbd6770954a7b57146
Hoang Nguyen <hn622@ic.ac.uk>
2023-07-03 18:56:37 +0100

    optimised lc0274 2

--- src/lc0274.java
+++ src/lc0274.java
@@ -6,7 +6,7 @@ public class lc0274 {
       int h = 0;
       for (int i = 0; i < citations.length; i++) {
         int val = 0;
-        if (citations[i] > h) {
+        if (citations[i] != 0 && citations[i] > h) {
           for (int j = 0; j < citations.length; j++) {
             if (citations[j] >= citations[i]) {
               val++;

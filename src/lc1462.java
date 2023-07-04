2cc206d9ebd442496c603011ed3e5e964249a4a6
Hoang Nguyen <hn622@ic.ac.uk>
2023-07-04 22:59:32 +0200

    change

---
+++ src/lc1462.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class lc1462 {
+  class Solution{
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequesites, int[][] queries) {
+      
+    }
+  }
+}
